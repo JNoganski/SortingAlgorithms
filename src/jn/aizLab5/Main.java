12377a73890d7ffb5686712027ccef3539c51ff5
JNoganski <noganski.jakub@gmail.com>
2021-01-25 23:03:41 +0100

    Selection sort added, three more to go;

--- src/jn/aizLab5/Main.java
+++ src/jn/aizLab5/Main.java
@@ -5,8 +5,11 @@ public class Main {
     public static void main(String[] args) {
     Sortowanie mojeSortowanie = new Sortowanie();
 
-      //  System.out.println(mojeSortowanie.losuj(100));
     mojeSortowanie.wypelnij(10,100);
+    mojeSortowanie.wypisz();
+
+    mojeSortowanie.selectionsort(false);
+        System.out.println("Poniżej posortowane ziooooom");
     mojeSortowanie.wypisz();
     }
 }
--- src/jn/aizLab5/Sortowanie.java
+++ src/jn/aizLab5/Sortowanie.java
@@ -9,7 +9,6 @@ public class Sortowanie extends SortZ {
         Random random = new Random();
         return random.nextInt(w_max - 0) + 0;
     }
-
     @Override
     public void wypelnij(int n, int wartosc_maksymalna) {
         ile =n;
@@ -18,7 +17,6 @@ public class Sortowanie extends SortZ {
         tab[i] = losuj(wartosc_maksymalna);
     }
     }
-
     @Override
     public void wypisz() {
 
@@ -26,15 +24,37 @@ public class Sortowanie extends SortZ {
             System.out.println(tab[i]);
         }
     }
-
     @Override
     public boolean compare(int a, int b, boolean rosnaco) {
-        return false;
+        // zwraca wynik por�wanania liczb a <= b lub a >= b
+        // w zale�no�ci od parametru rosnaco
+        if(rosnaco){
+            if(a<=b){
+                return true;
+            }else {
+                return false;
+            }
+        }else{
+            if (b<=a) {
+                return true;
+            }else {
+                return false;
+            }
+        }
     }
-
     @Override
     protected void selectionsort(boolean rosnaco) {
-
+        for(int i=0;i<ile-1;i++){
+            int min=i;
+            for(int j = i+1; j<ile;j++ ){
+                if(compare(tab[j],tab[min],rosnaco)){
+                    min = j;
+                }
+            }
+            int mmr = tab[min];
+            tab[min] = tab[i];
+            tab[i]=mmr;
+        }
     }
 
     @Override
