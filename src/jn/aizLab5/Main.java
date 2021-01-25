688966eea64d49021802ac6f0140c752207ee9b2
JNoganski <noganski.jakub@gmail.com>
2021-01-25 22:01:53 +0100

    Initial Commit v3 - the 'the proper One' Implemented methods wypelnij, losuj, wypisz correctly. All the implemented methods are inherited from abstract class SortZ, where they are abstract.

--- src/jn/aizLab5/Main.java
+++ src/jn/aizLab5/Main.java
@@ -3,6 +3,10 @@ package jn.aizLab5;
 public class Main {
 
     public static void main(String[] args) {
+    Sortowanie mojeSortowanie = new Sortowanie();
 
+      //  System.out.println(mojeSortowanie.losuj(100));
+    mojeSortowanie.wypelnij(10,100);
+    mojeSortowanie.wypisz();
     }
 }
--- src/jn/aizLab5/Sortowanie.java
+++ src/jn/aizLab5/Sortowanie.java
@@ -4,22 +4,27 @@ import java.util.Random;
 
 public class Sortowanie extends SortZ {
 
-    SortZ MySort = new Sortowanie();
-
     @Override
-    protected int losuj(int w_max) {
+    public int losuj(int w_max) {
         Random random = new Random();
         return random.nextInt(w_max - 0) + 0;
     }
 
     @Override
     public void wypelnij(int n, int wartosc_maksymalna) {
-
+        ile =n;
+    tab = new int[ile];
+    for(int i=0;i<n;i++){
+        tab[i] = losuj(wartosc_maksymalna);
+    }
     }
 
     @Override
     public void wypisz() {
 
+        for(int i=0;i<ile;i++){
+            System.out.println(tab[i]);
+        }
     }
 
     @Override
@@ -48,7 +53,7 @@ public class Sortowanie extends SortZ {
     }
 
     @Override
-    public void sortuj(boolean rosnaco, SortMethod metoda) {
+    public void sortuj(boolean rosnaco, SortMethod metoda){
 
     }
 }
